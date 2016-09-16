dd13ca1f9736b45924311693ec0c513d438586a3
Nikola Majcen <nikola.majcen@gmail.com>
2016-09-16 13:47:10 +0200

    Added file manager for saving and removing song from device

--- Cloud Player/Managers/FileManager.swift
+++ Cloud Player/Managers/FileManager.swift
@@ -10,11 +10,52 @@ import Foundation
 
 class FileManager {
     
+    // MARK: - Properties
+    
+    private let fileManager = NSFileManager.defaultManager()
+    private let directoryName = "iOSCloudPlayer"
+    
+    // MARK: - Public methods
+    
     func saveFile(name: String, data: NSData) -> String! {
-        return "/path/m.mp3"
+        let rootDirectoryPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,
+                                                                    .UserDomainMask, true).first! as NSString
+        let directoryPath = rootDirectoryPath.stringByAppendingPathComponent(directoryName) as NSString
+        if isMainDirectoryCreated(directoryPath as String) == false {
+            createMainDirectory(directoryPath as String)
+        }
+        
+        let path = directoryPath.stringByAppendingPathComponent(name + ".mp3")
+        if fileManager.createFileAtPath(path, contents: data, attributes: nil) == true {
+            return path
+        } else {
+            return nil
+        }
     }
     
     func removeFile(path: String) -> Bool {
-        return true
+        do {
+            try fileManager.removeItemAtPath(path)
+            return true
+        } catch let error as NSError {
+            print(error.description)
+            return false
+        }
+    }
+    
+    // MARK: - Private methods
+    
+    func createMainDirectory(path: String) {
+        do {
+            try fileManager.createDirectoryAtPath(path, withIntermediateDirectories: true, attributes: nil)
+        } catch let error as NSError {
+            print(error.description)
+        }
+    }
+    
+    func isMainDirectoryCreated(path: String) -> Bool {
+        var isDirectory : ObjCBool = false
+        let fileExists = fileManager.fileExistsAtPath(path, isDirectory: &isDirectory)
+        return fileExists && isDirectory
     }
 }
